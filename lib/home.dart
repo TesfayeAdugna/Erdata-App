02458aa415c31c57b4c62f3d1cf1b87dfbce98b7
ashrog <yohannesabel398@gmail.com>
2022-05-30 20:59:34 -0700

    unused imports

--- lib/home.dart
+++ lib/home.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:url_launcher/url_launcher_string.dart';
 import 'children.dart';
 import 'smallText.dart';
