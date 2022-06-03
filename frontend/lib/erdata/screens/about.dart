ec9d35c7d216c31208b8630326c83b255a5286fa
Kenna-Tefera <annatefera3@gmail.com>
2022-06-03 10:21:36 +0300

    editing UI

--- frontend/lib/erdata/screens/about.dart
+++ frontend/lib/erdata/screens/about.dart
@@ -27,12 +27,26 @@ class About extends StatelessWidget {
                           borderRadius: BorderRadius.all(Radius.circular(20)),
                           color:
                               Color.fromARGB(255, 71, 52, 52).withOpacity(0.8)),
-                      child: Text(
-                        "We are a non-governmental, non-profit organization founded on the ideals of humanism, freedom, equality and solidarity. We consider human dignity and freedom to be fundamental values. We  believe  that people anywhere in the world should have the right to make  decisions  about  their  lives  and to  share  the rights expressed in the Universal Declaration of Human Rights.",
-                        style: TextStyle(
-                            fontSize: 15,
-                            fontWeight: FontWeight.w400,
-                            color: Colors.white),
+                      child: Column(
+                        children: [
+                          Text(
+                            "About Us",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 194, 124, 3),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "We are a non-governmental, non-profit organization founded on the ideals of humanism, freedom, equality and solidarity. We consider human dignity and freedom to be fundamental values. We  believe  that people anywhere in the world should have the right to make  decisions  about  their  lives  and to  share  the rights expressed in the Universal Declaration of Human Rights.",
+                            style: TextStyle(
+                                fontSize: 15,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                        ],
                       ),
                     ),
                     Container(
@@ -42,13 +56,27 @@ class About extends StatelessWidget {
                         borderRadius: BorderRadius.all(Radius.circular(20)),
                         color: Color.fromARGB(255, 71, 52, 52).withOpacity(0.8),
                       ),
-                      child: Text(
-                        "We strive for a society that is open, informed, engaged and responsible with respect to problems at home and around the world.  We  want to actively  participate in  shaping a  society where cultural, ethnic, racial and other differences are a source of enrichment rather than conflict.",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Our Missions",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 194, 124, 3),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "We strive for a society that is open, informed, engaged and responsible with respect to problems at home and around the world.  We  want to actively  participate in  shaping a  society where cultural, ethnic, racial and other differences are a source of enrichment rather than conflict.",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                     Container(
@@ -58,13 +86,27 @@ class About extends StatelessWidget {
                           borderRadius: BorderRadius.all(Radius.circular(20)),
                           color:
                               Color.fromARGB(255, 71, 52, 52).withOpacity(0.8)),
-                      child: Text(
-                        "We are an intrinsic part of civil society. Our work is based on the personal initiative and activity of our team and volunteers, both in Addis Ababa and in the dozens of other cities where we operate, as well as the trust and support we are endowed with by individuals, businesses, governments and international institutions.",
-                        style: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w400,
-                          color: Colors.white,
-                        ),
+                      child: Column(
+                        children: [
+                          Text(
+                            "Our Dimensions",
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Color.fromARGB(255, 194, 124, 3),
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          ),
+                          SizedBox(height: 15),
+                          Text(
+                            "We  determine the  specific  areas and priorities of our activities with respect to the level of urgency and our actual capacity to change the situation. We cannot be everywhere, but it is essential that our work makes a real difference.",
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.w400,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
                   ],
