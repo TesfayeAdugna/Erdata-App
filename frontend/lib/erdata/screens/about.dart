435fc868ae0cef2467430582a409168bf4ca378f
Kenna-Tefera <annatefera3@gmail.com>
2022-06-03 09:26:52 +0300

    updating UI

--- frontend/lib/erdata/screens/about.dart
+++ frontend/lib/erdata/screens/about.dart
@@ -24,34 +24,48 @@ class About extends StatelessWidget {
                       margin: EdgeInsets.all(20),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromARGB(255, 180, 226, 249),
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color:
+                              Color.fromARGB(255, 71, 52, 52).withOpacity(0.8)),
                       child: Text(
-                          "We are a non-governmental, non-profit organization founded on the ideals of humanism, freedom, equality and solidarity. We consider human dignity and freedom to be fundamental values. We  believe  that people anywhere in the world should have the right to make  decisions  about  their  lives  and to  share  the rights expressed in the Universal Declaration of Human Rights."),
+                        "We are a non-governmental, non-profit organization founded on the ideals of humanism, freedom, equality and solidarity. We consider human dignity and freedom to be fundamental values. We  believe  that people anywhere in the world should have the right to make  decisions  about  their  lives  and to  share  the rights expressed in the Universal Declaration of Human Rights.",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white),
+                      ),
                     ),
                     Container(
                       margin: EdgeInsets.all(20),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromARGB(255, 180, 226, 249),
+                        borderRadius: BorderRadius.all(Radius.circular(20)),
+                        color: Color.fromARGB(255, 71, 52, 52).withOpacity(0.8),
                       ),
                       child: Text(
-                          "We strive for a society that is open, informed, engaged and responsible with respect to problems at home and around the world.  We  want to actively  participate in  shaping a  society where cultural, ethnic, racial and other differences are a source of enrichment rather than conflict.come and help the children.Hello Ethiopia, come and help the children.Hello Ethiopia, come and help the children.Hello Ethiopia, come and help the children."),
+                        "We strive for a society that is open, informed, engaged and responsible with respect to problems at home and around the world.  We  want to actively  participate in  shaping a  society where cultural, ethnic, racial and other differences are a source of enrichment rather than conflict.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                     Container(
                       margin: EdgeInsets.all(20),
                       padding: EdgeInsets.all(10),
                       decoration: BoxDecoration(
-                        border: Border.all(),
-                        borderRadius: BorderRadius.all(Radius.circular(10)),
-                        color: Color.fromARGB(255, 180, 226, 249),
-                      ),
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                          color:
+                              Color.fromARGB(255, 71, 52, 52).withOpacity(0.8)),
                       child: Text(
-                          "We are an intrinsic part of civil society. Our work is based on the personal initiative and activity of our team and volunteers, both in Addis Ababa and in the dozens of other cities where we operate, as well as the trust and support we are endowed with by individuals, businesses, governments and international institutions."),
+                        "We are an intrinsic part of civil society. Our work is based on the personal initiative and activity of our team and volunteers, both in Addis Ababa and in the dozens of other cities where we operate, as well as the trust and support we are endowed with by individuals, businesses, governments and international institutions.",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ],
                 ),
