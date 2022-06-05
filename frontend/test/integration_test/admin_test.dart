6b7113ca26452c63e3bd79f167a2ffc6921509a1
ashrog <yohannesabel398@gmail.com>
2022-06-05 01:35:11 -0700

    admin test

--- frontend/test/integration_test/admin_test.dart
+++ frontend/test/integration_test/admin_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:sec_2/admin/screens/admin.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:sec_2/admin/screens/admin.dart' as admin;
+import 'package:sec_2/admin/screens/admin_screen.dart';
 import 'package:sec_2/custom_widget/custom_widgets.dart';
 
 void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
   // Build our app and trigger a frame.
 
   Widget currentWidget() {
@@ -13,7 +16,9 @@ void main() {
     );
   }
 
-  testWidgets("Erdata admin title matches", (WidgetTester tester) async {
+  testWidgets("Erdata admin title matches", (tester) async {
+    admin.AdminScreen();
+    await tester.pumpAndSettle();
     await tester.pumpWidget(currentWidget());
     expect(find.text('Dashboard'), findsOneWidget);
   });
